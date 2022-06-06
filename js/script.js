4758402ae209465ae84d4f980249d175240390b3
Suja Tanvir Ahmed <30548517+SujaTanvirAhmed@users.noreply.github.com>
2022-06-06 15:17:41 +0600

    edited styles n added some mega menus

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const megaMenuCont = document.querySelector(".mega-menu-cont");
+const megaMenu = document.querySelector(".mega-menu");
+
+const btnFeatured = document.querySelector("#btn-featured");
+const btnOnSale = document.querySelector("#btn-onsale");
+const btnTopRated = document.querySelector("#btn-toprated");
+
+const btnAllDepartments = document.querySelector(".btn-all-departments");
+const menuAllDepartments = document.querySelector(".menu-all-departments");
+
+const contentFeatured = document.querySelector("#content-featured");
+const contentOnSale = document.querySelector("#content-onsale");
+const contentTopRated = document.querySelector("#content-toprated");
+
+btnAllDepartments.addEventListener("click", () => {
+    const displayStatus = menuAllDepartments.style.display;
+    if (displayStatus === "" || displayStatus === "none") {
+        menuAllDepartments.style.display = "flex";
+    }
+    else {
+        menuAllDepartments.style.display = "none";
+    }
+});
+
+megaMenuCont.addEventListener("click", () => {
+    megaMenu.style.display = "flex";
+});
+
+btnFeatured.addEventListener("click", () => {
+    let displayStatus = contentFeatured.style.display;
+    if (displayStatus === "" || displayStatus === "none") {
+        contentFeatured.style.display = "block";
+        contentOnSale.style.display = "none";
+        contentTopRated.style.display = "none";
+    }
+    else {
+        contentFeatured.style.display = "none";
+    }
+});
